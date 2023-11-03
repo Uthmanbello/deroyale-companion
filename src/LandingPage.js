93157c72603183ec383d694f6f4cc4704b548874
Uthmanbello <uthmanbello88@gmail.com>
2023-11-03 22:16:36 +0100

    Style the landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className='landing'>
-        <h2 className='landing-page-heading'><span style={{ fontWeight: 600, fontFamily: 'Josefin Sans, sans-serif', color: 'rgb(145, 194, 156)' }}>DeRoy<span style={{ color: '#ffb267' }} className="deroyale-at">@</span>l&#233; Lodge</span></h2>
+        <h2 className='landing-page-heading'>My Lodge</h2>
         <div className='lang-btns'>
           <Link to="/home/chat" className='lang-btn english-btn'>Get Started</Link>
         </div>    
