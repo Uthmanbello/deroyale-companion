4c65e36e67b1d89ecabb43961e199255e4aa9986
Uthmanbello <uthmanbello88@gmail.com>
2023-11-03 23:23:30 +0100

    Make input hidden after asnwering all questions

--- src/chat.js
+++ src/chat.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import './chat.css'
+import './chat.css';
 
 const ChatApp = () => {
   const [messages, setMessages] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [allQuestionsAnswered, setAllQuestionsAnswered] = useState(false);
   const questions = [
     "Welcome to our hotel! May I have your full name, please?",
     "Could you provide your home address and email for our records?",
@@ -15,9 +16,7 @@ const ChatApp = () => {
     "What are your check-in and check-out dates?",
     "Do you have any room preferences like smoking/non-smoking or bed type?",
     "Any additional requests or special needs during your stay?",
-    "May I have your consent to store your information as per data protection laws?",
-    "Thank you for the information! Is there anything else I can assist you with?"
-    // Add more questions as needed
+    "May I have your consent to store your information as per data protection laws?"
   ];
 
   const chatContainer = useRef(null);
@@ -30,20 +29,14 @@ const ChatApp = () => {
     }
   }, [currentQuestion]);
 
-  const scrollToBottom = () => {
-    chatContainer.current.scrollTop = chatContainer.current.scrollHeight;
-  };
-
   useEffect(() => {
     const scrollToBottom = () => {
       if (chatContainer.current) {
         chatContainer.current.scrollTop = chatContainer.current.scrollHeight;
       }
     };
-  
     scrollToBottom();
   }, [messages]);
-  
 
   const handleSendMessage = (message) => {
     setMessages((prevMessages) => [...prevMessages, { text: message, isUser: true }]);
@@ -55,6 +48,9 @@ const ChatApp = () => {
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
+    } else {
+      setAllQuestionsAnswered(true);
+      setMessages((prevMessages) => [...prevMessages, { text: "Thank you for the information!", isUser: false }]);
     }
   };
 
@@ -65,28 +61,30 @@ const ChatApp = () => {
           <h1 className='green'>Hotel Check-in</h1>
           <div ref={chatContainer} style={{ height: '65vh', overflowY: 'scroll', padding: '10px' }}>
             {messages.map((message, index) => (
-            <div key={index} style={{ textAlign: message.isUser ? 'right' : 'left', margin: '5px' }}>
+              <div key={index} style={{ textAlign: message.isUser ? 'right' : 'left', margin: '5px' }}>
                 <div style={{ padding: '10px', display: 'inline-block', borderRadius: '10px', backgroundColor: message.isUser ? '#122920' : 'rgb(145, 194, 156)', color: message.isUser ? '#ffffff' : '#000000' }}>
-                {message.text}
+                  {message.text}
                 </div>
-            </div>
+              </div>
             ))}
-        </div>
-        {currentQuestion < questions.length && (
-            <input
-            type="text"
-            className='reply-input'
-            placeholder="Type your reply..."
-            onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                handleSendMessage(e.target.value);
-                e.target.value = '';
-                }
-            }}
-            style={{ width: '100%', marginTop: '10px', marginBottom: '30px', padding: '20px', backgroundColor: 'rgb(230, 230, 230)', color: '#122920'}}
-            />
-        )}
-
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            {currentQuestion < questions.length && !allQuestionsAnswered && (
+              <input
+                type="text"
+                className='reply-input'
+                placeholder="Type your reply..."
+                onKeyPress={(e) => {
+                  if (e.key === 'Enter') {
+                    handleSendMessage(e.target.value);
+                    e.target.value = '';
+                  }
+                }}
+                style={{ width: '100%', marginTop: '10px', marginBottom: '30px', padding: '20px', backgroundColor: 'rgb(230, 230, 230)', color: '#122920'}}
+              />
+            )}
+            {allQuestionsAnswered && <div className='reply-input' style={{ padding: '20px', marginTop: '10px', marginBottom: '30px', backgroundColor: 'rgb(230, 230, 230)', color: '#122920' }}>All questions answered. We'll be in touch!</div>}
+          </div>
           <Link to="/home" className='lang-btn english-btn'>Back</Link>
         </div>
       </div>
@@ -95,3 +93,4 @@ const ChatApp = () => {
 };
 
 export default ChatApp;
+
